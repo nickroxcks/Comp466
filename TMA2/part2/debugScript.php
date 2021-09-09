bf6d812433c02e2129e813930a7702a67aa170e6
Nicholas <nicholas.serrano7@gmail.com>
2021-09-08 23:46:24 -0600

    debugging

--- TMA2/part2/debugScript.php
+++ TMA2/part2/debugScript.php
@@ -1,5 +1,32 @@
 <?php
 
+/*
+function sxe($url)
+{   
+    $xml = file_get_contents($url);
+    foreach ($http_response_header as $header)
+    {   
+        if (preg_match('#^Content-Type: text/xml; charset=(.*)#i', $header, $m))
+        {   
+            switch (strtolower($m[1]))
+            {   
+                case 'utf-8':
+                    // do nothing
+                    break;
+
+                case 'iso-8859-1':
+                    $xml = utf8_encode($xml);
+                    break;
+
+                default:
+                    $xml = iconv($m[1], 'utf-8', $xml);
+            }
+            break;
+        }
+    }
+
+    return simplexml_load_string($xml);
+}*/
 
 session_start();
 $userID = $_SESSION["idusers"];
@@ -17,7 +44,9 @@ libxml_use_internal_errors(true);
 if (file_exists($location)) {
     echo "going to try loading file";
     echo htmlentities($xmlString);
-    $xml = simplexml_load_string($xmlString);  //this is a SimpleXMLElement class. Note: __toString returns the data of the object
+    //$xml = sxe($location);  //this is a SimpleXMLElement class. Note: __toString returns the data of the object
+    $xml = iconv("GB18030", "utf-8", $xmlString);
+    $xml = simplexml_load_string($xml);
     foreach( libxml_get_errors() as $error ) {
 
         print_r($error);
