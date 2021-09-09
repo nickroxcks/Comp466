8d86097d5de21ed93dc3672330b39f2889d1fc9c
Nicholas <nicholas.serrano7@gmail.com>
2021-09-08 19:03:15 -0600

    debugging

--- TMA2/part2/debugScript.php
+++ TMA2/part2/debugScript.php
@@ -1,18 +1,19 @@
 <?php
 
-/*
+
 session_start();
 $userID = $_SESSION["idusers"];
 $location = 'upload/U_'.$_SESSION["idusers"].'_data.xml';
 
 
 if (file_exists($location)) {
+    echo "going to try loading file";
     $xml = simplexml_load_file($location);  //this is a SimpleXMLElement class. Note: __toString returns the data of the object
     echo "success";
 }
 else{
     echo "file not in path";
 }
-*/
+
 echo "test";
 ?>
