a0525c9093609e920c8c45b21395385a4967848e
Nicholas <nicholas.serrano7@gmail.com>
2021-09-08 23:16:48 -0600

    debugging

--- TMA2/part2/debugScript.php
+++ TMA2/part2/debugScript.php
@@ -4,16 +4,24 @@
 session_start();
 $userID = $_SESSION["idusers"];
 //$location = 'upload/U_'.$_SESSION["idusers"].'_data.xml';
-$location = 'http://143.198.71.129/part2/upload/U_12_data.xml';
+$location = 'upload/U_12_data.xml';
 
-/*
+
+
 if( ini_get('allow_url_fopen') ) {
     echo "allow url open is enabled...";
  }
 
+libxml_use_internal_errors(true);
+
 if (file_exists($location)) {
     echo "going to try loading file";
     $xml = simplexml_load_file($location);  //this is a SimpleXMLElement class. Note: __toString returns the data of the object
+    foreach( libxml_get_errors() as $error ) {
+
+        print_r($error);
+    
+    }
     echo "success";
 }
 else{
@@ -21,12 +29,13 @@ else{
 }
 
 echo "test";
-*/
+
+/*
 $url = $location;
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $xmlresponse = curl_exec($ch);
 $xml=simplexml_load_string($xmlresponse);
-print_r($xml);
+print_r($xml);*/
 ?>
