8ddde27bd672ec5465406a06ec323d508a235b61
Nicholas <nicholas.serrano7@gmail.com>
2021-09-12 15:43:27 -0600

    added another debuf file

---
+++ TMA2/part2/debugScript2.php
@@ -0,0 +1,42 @@
+<?php
+
+session_start();
+$userID = $_SESSION["idusers"];
+//$location = 'upload/U_'.$_SESSION["idusers"].'_data.xml';
+$location = "upload/U_12_data.xml";
+
+$xmlString = file_get_contents($location);
+
+if( ini_get('allow_url_fopen') ) {
+    echo "allow url open is enabled...";
+ }
+
+libxml_use_internal_errors(true);
+echo 'Current PHP version: ' . phpversion();
+if (file_exists($location)) {
+    echo "going to try loading file";
+    $dom = new DOMDocument;
+    $dom->loadXML($xmlString);
+    echo "success";
+    $Answers = $dom->getElementsByTagName('Answer');
+    foreach ($Answers as $Answer) {
+        echo $Answer->nodeValue, PHP_EOL;
+    }
+
+
+}
+else{
+    echo "file not in path";
+}
+
+echo "test";
+
+/*
+$url = $location;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$xmlresponse = curl_exec($ch);
+$xml=simplexml_load_string($xmlresponse);
+print_r($xml);*/
+?>
