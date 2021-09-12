0988ebf3f71e1667d49301a9a562fefc3a7bc6cb
Nicholas <nicholas.serrano7@gmail.com>
2021-09-12 16:00:36 -0600

    debugging

--- TMA2/part2/debugScript2.php
+++ TMA2/part2/debugScript2.php
@@ -14,8 +14,8 @@ if( ini_get('allow_url_fopen') ) {
 libxml_use_internal_errors(true);
 echo 'Current PHP version: ' . phpversion();
 if (file_exists($location)) {
-    echo "going to try loading file">;
-    echo $xmlString;
+    echo "going to try loading file";
+    echo htmlentities($xmlString);
     $dom = new DOMDocument;
     $dom->loadXML($xmlString);
     echo "success";
