f34eec6cff031d4000ab4ce6e9264a84a55086d5
Nicholas <nicholas.serrano7@gmail.com>
2021-09-12 16:19:18 -0600

    debugging

--- TMA2/part2/debugScript2.php
+++ TMA2/part2/debugScript2.php
@@ -1,48 +1,55 @@
-<?php
+<html>
+    <head>
+        <meta http-equiv="Cache-control" content="public">
+    </head>
+    <body>
+        <?php
 
-session_start();
-$userID = $_SESSION["idusers"];
-//$location = 'upload/U_'.$_SESSION["idusers"].'_data.xml';
-$location = "upload/U_12_data.xml";
+        session_start();
+        $userID = $_SESSION["idusers"];
+        //$location = 'upload/U_'.$_SESSION["idusers"].'_data.xml';
+        $location = "upload/U_12_data.xml";
 
-$xmlString = file_get_contents($location);
-set_error_handler(function($number, $error){
-    if (preg_match('/^DOMDocument::loadXML\(\): (.+)$/', $error, $m) === 1) {
-        throw new Exception($m[1]);
-    }
-});
+        $xmlString = file_get_contents($location);
+        set_error_handler(function($number, $error){
+            if (preg_match('/^DOMDocument::loadXML\(\): (.+)$/', $error, $m) === 1) {
+                throw new Exception($m[1]);
+            }
+        });
 
 
-if( ini_get('allow_url_fopen') ) {
-    echo "allow url open is enabled...";
- }
+        if( ini_get('allow_url_fopen') ) {
+            echo "allow url open is enabled...";
+        }
 
- echo 'Current PHP version: ' . phpversion();
-if (file_exists($location)) {
-    echo "going to try loading file";
-    echo htmlentities($xmlString);
-    $dom = new DOMDocument;
-    $dom->loadXML($xmlString);
-    restore_error_handler();
-    echo "success";
-    $Answers = $dom->getElementsByTagName('Answer');
-    foreach ($Answers as $Answer) {
-        echo $Answer->nodeValue, PHP_EOL;
-    }
-    
-}
-else{
-    echo "file not in path";
-}
+        echo 'Current PHP version: ' . phpversion();
+        if (file_exists($location)) {
+            echo "going to try loading file";
+            echo htmlentities($xmlString);
+            $dom = new DOMDocument;
+            $dom->loadXML($xmlString);
+            restore_error_handler();
+            echo "success";
+            $Answers = $dom->getElementsByTagName('Answer');
+            foreach ($Answers as $Answer) {
+                echo $Answer->nodeValue, PHP_EOL;
+            }
+            
+        }
+        else{
+            echo "file not in path";
+        }
 
-echo "test";
+        echo "test";
 
-/*
-$url = $location;
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$xmlresponse = curl_exec($ch);
-$xml=simplexml_load_string($xmlresponse);
-print_r($xml);*/
-?>
+        /*
+        $url = $location;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $xmlresponse = curl_exec($ch);
+        $xml=simplexml_load_string($xmlresponse);
+        print_r($xml);*/
+        ?>
+    </body>
+</html>
