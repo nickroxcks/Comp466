c2a59232830059df7bc50fc6db4225ff3e2de6f2
Nicholas <nicholas.serrano7@gmail.com>
2021-09-12 15:21:13 -0600

    debugging

--- TMA2/part2/debugScript.php
+++ TMA2/part2/debugScript.php
@@ -1,33 +1,5 @@
 <?php
 
-/*
-function sxe($url)
-{   
-    $xml = file_get_contents($url);
-    foreach ($http_response_header as $header)
-    {   
-        if (preg_match('#^Content-Type: text/xml; charset=(.*)#i', $header, $m))
-        {   
-            switch (strtolower($m[1]))
-            {   
-                case 'utf-8':
-                    // do nothing
-                    break;
-
-                case 'iso-8859-1':
-                    $xml = utf8_encode($xml);
-                    break;
-
-                default:
-                    $xml = iconv($m[1], 'utf-8', $xml);
-            }
-            break;
-        }
-    }
-
-    return simplexml_load_string($xml);
-}*/
-
 session_start();
 $userID = $_SESSION["idusers"];
 //$location = 'upload/U_'.$_SESSION["idusers"].'_data.xml';
@@ -40,7 +12,7 @@ if( ini_get('allow_url_fopen') ) {
  }
 
 libxml_use_internal_errors(true);
-
+echo 'Current PHP version: ' . phpversion();
 if (file_exists($location)) {
     echo "going to try loading file";
     echo htmlentities($xmlString);
