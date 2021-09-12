8d95ff894083edce528639df1840d66815c48400
Nicholas <nicholas.serrano7@gmail.com>
2021-09-12 16:05:30 -0600

    debugging

--- TMA2/part2/debugScript2.php
+++ TMA2/part2/debugScript2.php
@@ -6,25 +6,30 @@ $userID = $_SESSION["idusers"];
 $location = "upload/U_12_data.xml";
 
 $xmlString = file_get_contents($location);
+set_error_handler(function($number, $error){
+    if (preg_match('/^DOMDocument::loadXML\(\): (.+)$/', $error, $m) === 1) {
+        throw new Exception($m[1]);
+    }
+});
+
 
 if( ini_get('allow_url_fopen') ) {
     echo "allow url open is enabled...";
  }
 
-libxml_use_internal_errors(true);
-echo 'Current PHP version: ' . phpversion();
+ echo 'Current PHP version: ' . phpversion();
 if (file_exists($location)) {
     echo "going to try loading file";
     echo htmlentities($xmlString);
     $dom = new DOMDocument;
     $dom->loadXML($xmlString);
+    restore_error_handler();
     echo "success";
     $Answers = $dom->getElementsByTagName('Answer');
     foreach ($Answers as $Answer) {
         echo $Answer->nodeValue, PHP_EOL;
     }
-
-
+    
 }
 else{
     echo "file not in path";
